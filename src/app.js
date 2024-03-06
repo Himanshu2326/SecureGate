125228a80c08c4f9567401a3d49bc83f0a79db69
Himanshu Prajapati <127850749+Himanshu2326@users.noreply.github.com>
2024-03-06 13:59:25 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -48,40 +48,8 @@ app.get('/', (req, res) => {
     res.render("index")
 })
 
-
-
-
-//? Secret Page:-
-// app.get('/secret', auth, (req, res) => { // With this auth, it goes to middleware and check what happing , Jab Tak sab thik nahi hoga tab tak next func nahi chalega
-//     res.render("secret")
-//     console.log(`Your Cookie is : ${req.cookies.jwt}`);
-// })
-
-//? Logout :-
-// app.get('/logout', auth, async (req, res) => { // auth is checking for token is there or not
-
-
 app.get('/logout', async (req, res) => {
-
-    // try {
-    // For Single Logout:-
-    // req.User.tokens = req.User.tokens.filter((currentElement) => {
-    //     return currentElement.token !== req.token
-    // })
-
-    // For All Device Logout:-
-    // req.User.tokens = [];
-
-
-    // res.clearCookie("jwt");
-    console.log('Log out Succesfully');
-
-    // await req.User.save();
     res.render('index');
-
-    // } catch (error) {
-    // res.status(500).send(error)
-    // }
 })
 
 
@@ -117,26 +85,6 @@ app.post('/registration', async (req, res) => {
                 confirmpassword: req.body.confirmpassword,
             });
 
-            // Generating Tokens :-
-
-            // const token = await RegisterData.generateToken();
-            // console.log("The Token Is:", token);
-
-
-            /*
-             >> Generating Cookie:-
-
-             -> res.cookie function is used to set the cookie name to Value.
-             -> The Value parameter may be a String or Object Converted to JSON.
-            
-            */
-            // res.cookie("jwt", token, {
-            //     expires: new Date(Date.now + 30000),
-            //     httpOnly: true
-            // });
-            // console.log(cookie);
-
-            //   Saving Registered Data:--
             const Save = await RegisterData.save();
             res.status(201).render("Home")
         }
@@ -158,45 +106,20 @@ app.post('/login', async (req, res) => {
         const EnterPassword = req.body.password;
 
         const UserEmail = await Registration.findOne({ email: EnterEmail });
-        // const UserName = await Registration.findOne({'email': 'ku@gmail.com'},{'firstname':1, _id:0})
-
-
-        // Generating Tokens:-
-        // const token = await RegisterData.generateToken();
-
-        // console.log("Token:", Token)
-
-        // Generating Cookies:-
-        // res.cookie("jwt", token, {
-        //     expires: new Date(Date.now + 30000),
-        //     httpOnly: true
-        // });
-
-        //   >>   Using bcrypt
-
-        // const isEmail = await bcrypt.compare(EnterPassword, UserEmail.password);  // For Checking brypt , Note Password Should Store  In bcrypt 
-        // console.log(isEmail);
-
-        // if (isEmail) {
-        //     res.status(201).render("index");
-        // }
-
-        // console.log(EnterPassword);
-        // console.log(UserEmail.password);
-        console.log(`Entered : ${EnterEmail}`);
-        console.log(`Saved : ${UserEmail.email}`);
-
-
-        // let WelcomeName = UserName.firstname;
-
-
-        if (EnterPassword === UserEmail.password) {
+        
+         if (!UserData) {
+            res.status(400).render("Invalid");
+        }
+        else if (UserData.EnterEmail !== null && EnterPassword === UserData.password) {
             res.status(201).render("Home");
-
+        }
+        else if (UserData.EnterEmail !== EnterEmail) {
+            res.status(201).render("Invalid");
         }
         else {
             res.status(400).render("Invalid");
         }
+        
     } catch (error) {
         res.status(400).send(error);
     }
